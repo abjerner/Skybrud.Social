df01af20d60ac29e0fc69df6444833371e3fde1e
Anders Bjerner <abjerner@skybrud.dk>
2013-10-02 23:23:01 +0200

    Instagram: Added methods for caching purposes

--- Skybrud.Social/Skybrud.Social/Instagram/Responses/InstagramRecentMediaResponse.cs
+++ Skybrud.Social/Skybrud.Social/Instagram/Responses/InstagramRecentMediaResponse.cs
@@ -1,10 +1,16 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using Skybrud.Social.Instagram.Objects;
 using Skybrud.Social.Json;
 
 namespace Skybrud.Social.Instagram.Responses {
 
     public class InstagramRecentMediaResponse : InstagramResponse {
+
+        /// <summary>
+        /// Gets the internal JsonObject the object was created from.
+        /// </summary>
+        public JsonObject JsonObject { get; private set; }
 
         /// <summary>
         /// Gets an array of all media in the response.
@@ -35,10 +41,39 @@ namespace Skybrud.Social.Instagram.Responses {
         public string NextUrl { get; private set; }
         public string NextMaxId { get; private set; }
 
+        #region Member methods
+
+        /// <summary>
+        /// Gets a JSON string representing the user.
+        /// </summary>
+        public string ToJson() {
+            return JsonObject == null ? null : JsonObject.ToJson();
+        }
+
+        /// <summary>
+        /// Save the response to a JSON file at the specified <var>path</var>.
+        /// </summary>
+        /// <param name="path">The path to save the file.</param>
+        public void SaveJson(string path) {
+            File.WriteAllText(path, ToJson());
+        }
+
         public InstagramRecentMediaResponse GetNextPage() {
             return NextUrl == null ? null : Parse(SocialUtils.DoHttpGetRequestAndGetBodyAsJsonObject(NextUrl));
         }
 
+        #endregion
+
+        #region Static methods
+
+        /// <summary>
+        /// Load a response from the JSON file at the specified <var>path</var>.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        public static InstagramRecentMediaResponse LoadJson(string path) {
+            return ParseJson(File.ReadAllText(path));
+        }
+        
         public static InstagramRecentMediaResponse ParseJson(string json) {
             return JsonConverter.ParseObject(json, Parse);
         }
@@ -56,12 +91,15 @@ namespace Skybrud.Social.Instagram.Responses {
 
             // Parse the response
             return new InstagramRecentMediaResponse {
+                JsonObject = obj,
                 NextUrl = pagination == null ? null : pagination.GetString("next_url"),
                 NextMaxId = pagination == null ? null : pagination.GetString("next_max_id"),
                 Data = InstagramMedia.ParseMultiple(obj.GetArray("data"))
             };
 
         }
+
+        #endregion
 
     }
 
--- Skybrud.Social/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
+++ Skybrud.Social/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.0.428.26")]
+[assembly: AssemblyFileVersion("0.0.429.3")]
 
