40da29c73213b001314eb5943682820e63e5169a
Anders Bjerner <abjerner@skybrud.dk>
2013-10-14 23:42:57 +0200

    Facebook: Added JSON cache/serialization methods to FacebookPostSummary class

--- Skybrud.Social/Skybrud.Social/Facebook/Objects/FacebookPostSummary.cs
+++ Skybrud.Social/Skybrud.Social/Facebook/Objects/FacebookPostSummary.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using Skybrud.Social.Interfaces;
 using Skybrud.Social.Json;
+using Skybrud.Social.Twitter.Objects;
 
 namespace Skybrud.Social.Facebook.Objects {
 
     public class FacebookPostSummary : ISocialTimelineEntry {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the internal JsonObject the object was created from.
+        /// </summary>
+        public JsonObject JsonObject { get; private set; }
 
         public string Id { get; private set; }
         public FacebookIdName From { get; private set; }
@@ -26,9 +34,67 @@ namespace Skybrud.Social.Facebook.Objects {
         public FacebookLikes Likes { get; private set; }
         public FacebookComments Comments { get; private set; }
 
+        public DateTime SortDate {
+            get { return CreatedTime; }
+        }
+
+        #endregion
+
+        #region Constructor(s)
+
+        private FacebookPostSummary() {
+            // Hide default constructor
+        }
+
+        #endregion
+
+        #region Member methods
+
+        /// <summary>
+        /// Gets a JSON string representing the object.
+        /// </summary>
+        public string ToJson() {
+            return JsonObject == null ? null : JsonObject.ToJson();
+        }
+
+        /// <summary>
+        /// Saves the object to a JSON file at the specified <var>path</var>.
+        /// </summary>
+        /// <param name="path">The path to save the file.</param>
+        public void SaveJson(string path) {
+            if (JsonObject != null) JsonObject.SaveJson(path);
+        }
+
+        #endregion
+
+
+        #region Static methods
+
+        /// <summary>
+        /// Loads a post from the JSON file at the specified <var>path</var>.
+        /// </summary>
+        /// <param name="path">The path to the file.</param>
+        public static FacebookPostSummary LoadJson(string path) {
+            return JsonObject.LoadJson(path, Parse);
+        }
+
+        /// <summary>
+        /// Gets a post from the specified JSON string.
+        /// </summary>
+        /// <param name="json">The JSON string representation of the object.</param>
+        public static FacebookPostSummary ParseJson(string json) {
+            return JsonObject.ParseJson(json, Parse);
+        }
+
+        /// <summary>
+        /// Gets a post from the specified <var>JsonObject</var>.
+        /// </summary>
+        /// <param name="obj">The instance of <var>JsonObject</var> to parse.</param>
+
         public static FacebookPostSummary Parse(JsonObject obj) {
             if (obj == null) return null;
             return new FacebookPostSummary {
+                JsonObject = obj,
                 Id = obj.GetString("id"),
                 From = obj.GetObject("from", FacebookIdName.Parse),
                 Application = obj.GetObject("application", FacebookIdName.Parse),
@@ -55,10 +121,8 @@ namespace Skybrud.Social.Facebook.Objects {
             return array == null ? new FacebookPostSummary[0] : array.ParseMultiple(Parse);
         }
 
-        public DateTime SortDate {
-            get { return CreatedTime; }
-        }
-    
+        #endregion
+
     }
 
 }
