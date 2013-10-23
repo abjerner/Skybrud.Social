442629dc2576c9fc79237fbde915f222202ee126
Anders Bjerner <abjerner@skybrud.dk>
2013-10-23 23:22:06 +0200

    Instagram: Adjusted the InstagramScope class/enum a bit since the "basic" scope is given by default

--- Skybrud.Social/Skybrud.Social/Instagram/InstagramScope.cs
+++ Skybrud.Social/Skybrud.Social/Instagram/InstagramScope.cs
@@ -4,10 +4,27 @@ namespace Skybrud.Social.Instagram {
 
     [Flags]
     public enum InstagramScope {
-        Basic = 1,
-        Comments = 2,
-        Relationships = 4,
-        Likes = 8
+
+        /// <summary>
+        /// To read any and all data related to a user (e.g. following/followed-by lists, photos, etc.) (granted by default).
+        /// </summary>
+        Basic = 0,
+
+        /// <summary>
+        /// To create or delete comments on a user’s behalf.
+        /// </summary>
+        Comments = 1,
+
+        /// <summary>
+        /// To follow and unfollow users on a user’s behalf.
+        /// </summary>
+        Relationships = 2,
+
+        /// <summary>
+        /// To like and unlike items on a user’s behalf
+        /// </summary>
+        Likes = 4
+    
     }
 
 }
--- Skybrud.Social/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
+++ Skybrud.Social/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.0.449.8")]
+[assembly: AssemblyFileVersion("0.0.450.1")]
 
