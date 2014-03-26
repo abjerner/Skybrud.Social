330d2380439eabc1fc459792fdc1f9567fc250af
Anders Bjerner <abjerner@skybrud.dk>
2014-03-27 00:31:56 +0100

    Added classes SocialHttpRequest and SocialHttpResponse since some services needs extra stuff like headers or basic authentication. Still work in progress though...

---
+++ src/Skybrud.Social/SocialHttpRequest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Skybrud.Social {
+    
+    public class SocialHttpRequest {
+
+        #region Private fields
+
+        private NameValueCollection _headers = new NameValueCollection();
+        private NameValueCollection _queryString = new NameValueCollection();
+        private NameValueCollection _postData = new NameValueCollection();
+
+        #endregion
+
+        #region Properties
+
+        public string Method { get; set; }
+        public NetworkCredential Credentials { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL of the request. The query string can either be specified directly
+        /// in the URL, or separately through the <var>QueryString</var> property.
+        /// </summary>
+        public string Url { get; set; }
+
+        /// <summary>
+        /// Gets or sets the encoding of the request. Default is UTF-8.
+        /// </summary>
+        public Encoding Encoding { get; set; }
+
+        public NameValueCollection Headers {
+            get { return _headers; }
+            set { _headers = value ?? new NameValueCollection(); }
+        }
+
+        public NameValueCollection QueryString {
+            get { return _queryString; }
+            set { _queryString = value ?? new NameValueCollection(); }
+        }
+
+        public NameValueCollection PostData {
+            get { return _postData; }
+            set { _postData = value ?? new NameValueCollection(); }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public SocialHttpRequest() {
+            Method = "GET";
+            Encoding = Encoding.UTF8;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public SocialHttpResponse GetResponse() {
+            
+            // Merge the query string
+            string url = new UriBuilder(Url).MergeQueryString(QueryString).ToString();
+
+            // Initialize the request
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+
+            // Set the method
+            request.Method = Method;
+            request.Credentials = Credentials;
+
+            // Add any headers
+            // TODO: Add headers
+
+            // Add the request body (if a POST request)
+            if (Method == "POST" && PostData != null && PostData.Count > 0) {
+                string dataString = SocialUtils.NameValueCollectionToQueryString(PostData);
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = dataString.Length;
+                using (Stream stream = request.GetRequestStream()) {
+                    stream.Write(Encoding.UTF8.GetBytes(dataString), 0, dataString.Length);
+                }
+            }
+
+            // Get the response
+            try {
+                return new SocialHttpResponse((HttpWebResponse) request.GetResponse());
+            } catch (WebException ex) {
+                return new SocialHttpResponse((HttpWebResponse) ex.Response, ex);
+            }
+
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/Skybrud.Social/SocialHttpResponse.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+using System.Net;
+using Skybrud.Social.Json;
+
+namespace Skybrud.Social {
+    
+    public class SocialHttpResponse {
+
+        public HttpWebResponse Response { get; private set; }
+        public WebException Exception { get; private set; }
+
+        public HttpStatusCode StatusCode {
+            get { return Response.StatusCode; }
+        }
+
+        public string StatusDescription {
+            get { return Response.StatusDescription; }
+        }
+
+        public string Method {
+            get { return Response.Method; }
+        }
+
+        public string ContentType {
+            get { return Response.ContentType; }
+        }
+
+        public WebExceptionStatus WebStatus {
+            get { return Exception == null ? WebExceptionStatus.Success : Exception.Status; }
+        }
+        
+        internal SocialHttpResponse(HttpWebResponse response) {
+            Response = response;
+        }
+
+        internal SocialHttpResponse(HttpWebResponse response, WebException exception) {
+            Response = response;
+            Exception = exception;
+        }
+
+        /// <summary>
+        /// Gets the response body as a RAW string.
+        /// </summary>
+        public string GetBodyAsString() {
+            using (Stream stream = Response.GetResponseStream()) {
+                using (StreamReader reader = new StreamReader(stream)) {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the response body as an instance of either <var>JsonObject</var> or
+        /// <var>JsonArray</var>.
+        /// </summary>
+        public IJsonObject GetBodyAsJson() {
+            return JsonConverter.Parse(GetBodyAsString());
+        }
+
+        /// <summary>
+        /// Gets the response body as an instance of <var>JsonObject</var>.
+        /// </summary>
+        public JsonObject GetBodyAsJsonObject() {
+            return GetBodyAsJson() as JsonObject;
+        }
+
+        /// <summary>
+        /// Gets the response body as an instance of <var>JsonArray</var>.
+        /// </summary>
+        public JsonArray GetBodyAsJsonArray() {
+            return GetBodyAsJson() as JsonArray;
+        }
+
+    }
+
+}
