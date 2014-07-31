4b991edcead818c6aa01f123350490abc6adb915
Anders Bjerner <abjerner@skybrud.dk>
2014-08-01 01:06:45 +0200

    Twitter: Added logic for formatting at text with entities (may not be done yet)

---
+++ src/Skybrud.Social/Twitter/Entities/Formatting/TwitterExtendedEntityFormatter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Skybrud.Social.Twitter.Entities.Formatting {
+    
+    public class TwitterExtendedEntityFormatter : TwitterDefaultEntityFormatter {
+
+        public Func<string, TwitterHashTagEntity, string> Tags { get; set; }
+        public Func<string, TwitterUrlEntity, string> Urls { get; set; }
+        public Func<string, TwitterMentionEntity, string> Mentions { get; set; }
+        public Func<string, TwitterMediaEntity, string> Media { get; set; }
+
+        /// <summary>
+        /// Formats a hashtag in a status message, description or similar.
+        /// </summary>
+        /// <param name="text">The text being replaced.</param>
+        /// <param name="tag">The hashtag.</param>
+        public override string FormatTag(string text, TwitterHashTagEntity tag) {
+            return Tags == null ? base.FormatTag(text, tag) : Tags(text, tag);
+        }
+
+        /// <summary>
+        /// Formats an URL in a status message, description or similar.
+        /// </summary>
+        /// <param name="text">The text being replaced.</param>
+        /// <param name="url">The URL.</param>
+        public override string FormatUrl(string text, TwitterUrlEntity url) {
+            return Urls == null ? base.FormatUrl(text, url) : Urls(text, url);
+        }
+
+        /// <summary>
+        /// Formats a user mention in a status message, description or similar.
+        /// </summary>
+        /// <param name="text">The text being replaced.</param>
+        /// <param name="mention"></param>
+        public override string FormatMention(string text, TwitterMentionEntity mention) {
+            return Mentions == null ? base.FormatMention(text, mention) : Mentions(text, mention);
+        }
+
+        /// <summary>
+        /// Formats a media in a status message, description or similar.
+        /// </summary>
+        /// <param name="text">The text being replaced.</param>
+        /// <param name="media">The media.</param>
+        public override string FormatMedia(string text, TwitterMediaEntity media) {
+            return Media == null ? base.FormatMedia(text, media) : Media(text, media);
+        }
+
+    }
+
+}
--- src/Skybrud.Social/Twitter/TwitterUtils.cs
+++ src/Skybrud.Social/Twitter/TwitterUtils.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using Skybrud.Social.Twitter.Entities;
+using Skybrud.Social.Twitter.Entities.Formatting;
 using Skybrud.Social.Twitter.Enums;
 
 namespace Skybrud.Social.Twitter {
@@ -24,7 +28,54 @@ namespace Skybrud.Social.Twitter {
                 default: throw new Exception("Unknown granularity \"" + str + "\"");
             }
         }
-    
+
+        #region FormatEntities(...)
+
+
+
+        public static string FormatEntities(string text, IEnumerable<TwitterBaseEntity> entities) {
+            return FormatEntities(text, entities, new TwitterDefaultEntityFormatter());
+        }
+
+        public static string FormatEntities(string text, IEnumerable<TwitterBaseEntity> entities, ITwitterEntityFormatter formatter) {
+
+            // Some input validation
+            if (String.IsNullOrWhiteSpace(text) || entities == null || formatter == null) return text;
+
+            // Iterate through the entities
+            foreach (TwitterBaseEntity entity in entities.OrderByDescending(x => x.StartIndex)) {
+
+                string before = text.Substring(0, entity.StartIndex);
+                string current = text.Substring(entity.StartIndex, entity.EndIndex - entity.StartIndex);
+                string after = text.Substring(entity.EndIndex);
+
+                string formatted = null;
+
+                TwitterHashTagEntity hashtag = entity as TwitterHashTagEntity;
+                TwitterUrlEntity url = entity as TwitterUrlEntity;
+                TwitterMentionEntity mention = entity as TwitterMentionEntity;
+                TwitterMediaEntity media = entity as TwitterMediaEntity;
+
+                if (hashtag != null) {
+                    formatted = formatter.FormatTag(current, hashtag);
+                } else if (url != null) {
+                    formatted = formatter.FormatUrl(current, url);
+                } else if (mention != null) {
+                    formatted = formatter.FormatMention(current, mention);
+                } else if (media != null) {
+                    formatted = formatter.FormatMedia(current, media);
+                }
+
+                if (formatted != null) text = before + formatted + after;
+
+            }
+
+            return text;
+
+        }
+
+        #endregion
+
     }
 
 }
