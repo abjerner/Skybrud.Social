77e64faffc8889573de8b1abf4894021bb826047
Anders Bjerner <abjerner@skybrud.dk>
2014-08-31 16:36:19 +0200

    Added some more more to the SocialJsonObject class
    - Added constructor taking the JsonObject as it's only parameter. I usually forget to set the property, so now I have to call the constructor.
    - Added ShouldSerializeJsonObject() method to play nice with JSON.net
    - Added ToJson() and and SaveJson() methods. These methods have until now been implemented individually in each class, but with the introduction of the SocialJsonObject class, it makes most sence to move them here

--- src/Skybrud.Social/SocialJsonObject.cs
+++ src/Skybrud.Social/SocialJsonObject.cs
@@ -4,10 +4,53 @@ namespace Skybrud.Social {
 
     public abstract class SocialJsonObject {
 
+        #region Properties
+
         /// <summary>
         /// Gets the internal JsonObject the object was created from.
         /// </summary>
-        public JsonObject JsonObject { get; protected set; }
+        public JsonObject JsonObject { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        protected SocialJsonObject(JsonObject obj) {
+            JsonObject = obj;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// This method is only here to make sure that the JsonObject property
+        /// isn't serialized by the <code>JSON.net</code> package. By having
+        /// this method rather than using the <code>JsonIgnore</code>
+        /// attribute, we don't have to create any references to the
+        /// <code>JSON.net</code> package.
+        /// </summary>
+        private bool ShouldSerializeJsonObject() {
+            return false;
+        }
+
+        /// <summary>
+        /// Generates a JSON string representing the object.
+        /// </summary>
+        public virtual string ToJson() {
+            return JsonObject == null ? null : JsonObject.ToJson();
+        }
+
+        /// <summary>
+        /// Saves the object to a JSON file at the specified <var>path</var>.
+        /// </summary>
+        /// <param name="path">The path to save the file.</param>
+        public virtual void SaveJson(string path) {
+            if (JsonObject != null) JsonObject.SaveJson(path);
+        }
+
+        #endregion
+
     }
 
 }
