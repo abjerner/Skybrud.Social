4b5eff49e287aac58a9056e80eb16d74410d9681
Anders Bjerner <abjerner@skybrud.dk>
2014-10-15 23:45:21 +0200

    GitHub: Updated GitHubHttpException to better reflect the API response

--- src/Skybrud.Social/GitHub/Exceptions/GitHubHttpException.cs
+++ src/Skybrud.Social/GitHub/Exceptions/GitHubHttpException.cs
@@ -1,15 +1,24 @@
 using System.Net;
 
 namespace Skybrud.Social.GitHub.Exceptions {
-    
+
     public class GitHubHttpException : GitHubException {
 
         public HttpStatusCode StatusCode { get; private set; }
 
+        public string DocumentationUrl { get; private set; }
+
         public GitHubHttpException(HttpStatusCode status) : base("Invalid response received from the GitHub API (Status: " + ((int) status) + ")") {
-
             StatusCode = status;
+        }
 
+        public GitHubHttpException(HttpStatusCode status, string message) : base(message) {
+            StatusCode = status;
+        }
+
+        public GitHubHttpException(HttpStatusCode status, string message, string documentationUrl) : base(message) {
+            StatusCode = status;
+            DocumentationUrl = documentationUrl;
         }
 
     }
