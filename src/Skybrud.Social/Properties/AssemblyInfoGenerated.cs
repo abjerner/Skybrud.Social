2bad4477bc8089da079287f39f485d4df32ed531
Anders Bjerner <abjerner@skybrud.dk>
2014-10-29 23:41:28 +0100

    Added some extra methods to the SocialUtils class (mostly for internal use)

--- src/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
+++ src/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.0.820.3")]
+[assembly: AssemblyFileVersion("0.0.821.1")]
 
--- src/Skybrud.Social/SocialUtils.cs
+++ src/Skybrud.Social/SocialUtils.cs
@@ -33,6 +33,40 @@ namespace Skybrud.Social {
             return FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
         }
 
+        /// <summary>
+        /// Gets the amount of days between the date of this build and the date the project was started - that is the 30th of July, 2012.
+        /// </summary>
+        public static int GetDaysSinceStart() {
+
+            // Get the third bit as a string
+            string str = GetFileVersion().Split('.')[2];
+
+            // Parse the string into an integer
+            return Int32.Parse(str);
+
+        }
+
+        /// <summary>
+        /// Gets the date of this build of Skybrud.Social.
+        /// </summary>
+        public static DateTime GetBuildDate() {
+            return new DateTime(2012, 7, 30).AddDays(GetDaysSinceStart());
+        }
+
+        /// <summary>
+        /// Gets the build number of the day. This is mostly used for internal
+        /// purposes to distinguish builds with the same assembly version.
+        /// </summary>
+        public static int GetBuildNumber() {
+
+            // Get the fourth bit as a string
+            string str = GetFileVersion().Split('.')[2];
+
+            // Parse the string into an integer
+            return Int32.Parse(str);
+
+        }
+
         #region HTTP helpers
 
         private static HttpWebResponse DoHttpRequest(string url, string method, NameValueCollection queryString, NameValueCollection postData) {
