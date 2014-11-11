f352b930c576a2932a14a7f66d844df5924f2554
Anders Bjerner <abjerner@skybrud.dk>
2014-11-11 22:31:21 +0100

    Twitter: Various changes to the TwitterStatusMessage class
    Amongst a few changes, this adds the FavoriteCount property as reported in #9. Also added, renamed and removed some properties, so this commit is yet a breaking change.

--- src/Skybrud.Social/Twitter/Objects/TwitterStatusMessage.cs
+++ src/Skybrud.Social/Twitter/Objects/TwitterStatusMessage.cs
@@ -15,11 +15,6 @@ namespace Skybrud.Social.Twitter.Objects {
         public long Id { get; private set; }
 
         /// <summary>
-        /// The ID of the tweet as a string.
-        /// </summary>
-        public string IdStr { get; private set; }
-
-        /// <summary>
         /// The date and time for when the tweet was created in UTC/GMT.
         /// </summary>
         public DateTime CreatedAt { get; private set; }
@@ -33,21 +28,24 @@ namespace Skybrud.Social.Twitter.Objects {
         /// The source/client used for making the tweet.
         /// </summary>
         public string Source { get; private set; }
+        
         public bool IsTruncated { get; private set; }
 
         public TwitterReplyTo InReplyTo { get; private set; }
 
-        public int Retweets { get; private set; }
+        public int RetweetCount { get; private set; }
+
+        public int FavoriteCount { get; private set; }
 
         /// <summary>
-        /// Gets whether the current user has favorited the tweet.
+        /// Gets whether the authenticated user has favorited the tweet.
         /// </summary>
-        public bool Favorited { get; private set; }
+        public bool HasFavorited { get; private set; }
         
         /// <summary>
-        /// Gets whether the current user has retweeted the tweet.
+        /// Gets whether the authenticated user has retweeted the tweet.
         /// </summary>
-        public bool Retweeted { get; private set; }
+        public bool HasRetweeted { get; private set; }
 
         public TwitterStatusMessageEntities Entities { get; private set; }
 
@@ -59,6 +57,10 @@ namespace Skybrud.Social.Twitter.Objects {
         /// Information about the user who made the tweet. May be <code>NULL</code>.
         /// </summary>
         public TwitterUser User { get; private set; }
+
+        public bool IsPossiblyOffensive { get; private set; }
+
+        public string Language { get; private set; }
 
         public DateTime SortDate {
             get { return CreatedAt; }
@@ -105,7 +107,6 @@ namespace Skybrud.Social.Twitter.Objects {
 
             TwitterStatusMessage msg = new TwitterStatusMessage(obj) {
                 Id = obj.GetInt64("id"),
-                IdStr = obj.GetString("id_str"),
                 Text = obj.GetString("text"),
                 Source = obj.GetString("source"),
                 IsTruncated = obj.GetBoolean("truncated")
@@ -125,14 +126,15 @@ namespace Skybrud.Social.Twitter.Objects {
                 };
             }
 
-            msg.Retweets = obj.GetInt32("retweet_count");
+            msg.RetweetCount = obj.GetInt32("retweet_count");
+            msg.FavoriteCount = obj.GetInt32("favorite_count");
 
             // Related to the authenticating user
-            msg.Favorited = obj.GetBoolean("favorited");
-            msg.Retweeted = obj.GetBoolean("retweeted");
+            msg.HasFavorited = obj.GetBoolean("favorited");
+            msg.HasRetweeted = obj.GetBoolean("retweeted");
 
             // Parse the entities (if any)
-            msg.Entities = TwitterStatusMessageEntities.Parse(obj.GetObject("entities"));
+            msg.Entities = obj.GetObject("entities", TwitterStatusMessageEntities.Parse);
 
             // For some weird reason Twitter flips the coordinates by writing longitude before latitude
             // See: https://dev.twitter.com/docs/platform-objects/tweets#obj-coordinates)
@@ -152,6 +154,9 @@ namespace Skybrud.Social.Twitter.Objects {
 
             msg.User = obj.GetObject("user", TwitterUser.Parse);
             msg.Place = obj.GetObject("place", TwitterPlace.Parse);
+
+            msg.IsPossiblyOffensive = obj.GetBoolean("possibly_sensitive");
+            msg.Language = obj.GetString("lang");
 
             return msg;
 
