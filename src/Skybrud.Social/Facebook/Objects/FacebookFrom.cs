1c1195badca21aefd2766dadfc1c6443ea71fc2b
Anders Bjerner <abjerner@skybrud.dk>
2014-11-22 16:52:39 +0100

    Facebook: Introduced class FacebookFrom

---
+++ src/Skybrud.Social/Facebook/Objects/FacebookFrom.cs
@@ -0,0 +1,54 @@
+﻿using Skybrud.Social.Json;
+
+namespace Skybrud.Social.Facebook.Objects {
+
+    public class FacebookFrom : SocialJsonObject {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the ID of the user or page.
+        /// </summary>
+        public string Id { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the user or page.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the primary category of a page. Is <code>NULL</code> for users.
+        /// </summary>
+        public string Category { get; private set; }
+
+        /// <summary>
+        /// Gets list of sub categories of a page. Is <code>NULL</code> for users.
+        /// </summary>
+        public FacebookObject[] CategoryList { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        private FacebookFrom(JsonObject obj) : base(obj) { }
+
+        #endregion
+
+        #region Static methods
+
+        public static FacebookFrom Parse(JsonObject obj) {
+            if (obj == null) return null;
+            return new FacebookFrom(obj) {
+                Id = obj.GetString("id"),
+                Name = obj.GetString("name"),
+                Category = obj.GetString("category"),
+                CategoryList = obj.GetArray("category_list", FacebookObject.Parse)
+            };
+
+        }
+
+        #endregion
+
+    }
+
+}
