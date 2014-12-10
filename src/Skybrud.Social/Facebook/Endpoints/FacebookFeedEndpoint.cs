09be10f0885d8e23c57f8b540cfea75b15e0d8c0
Anders Bjerner <abjerner@skybrud.dk>
2014-12-10 23:14:26 +0100

    Facebook: Implemented the "Feed" endpoint

---
+++ src/Skybrud.Social/Facebook/Endpoints/FacebookFeedEndpoint.cs
@@ -0,0 +1,60 @@
+using Skybrud.Social.Facebook.Collections;
+using Skybrud.Social.Facebook.Endpoints.Raw;
+using Skybrud.Social.Facebook.Options;
+using Skybrud.Social.Facebook.Responses;
+
+namespace Skybrud.Social.Facebook.Endpoints {
+    
+    public class FacebookFeedEndpoint {
+
+        /// <summary>
+        /// A reference to the Facebook service.
+        /// </summary>
+        public FacebookService Service { get; private set; }
+
+        /// <summary>
+        /// A reference to the raw endpoint.
+        /// </summary>
+        public FacebookFeedRawEndpoint Raw {
+            get { return Service.Client.Feed; }
+        }
+
+        internal FacebookFeedEndpoint(FacebookService service) {
+            Service = service;
+        }
+
+        #region Methods
+
+        /// <summary>
+        /// Gets the events of the specified user or page.
+        /// </summary>
+        /// <param name="identifier">The ID or name of the user/page.</param>
+        public FacebookResponse<FacebookFeedCollection> GetEvents(string identifier) {
+            return GetEvents(identifier, new FacebookFeedOptions());
+        }
+
+        /// <summary>
+        /// Gets the events of the specified user or page.
+        /// </summary>
+        /// <param name="identifier">The ID or name of the user/page.</param>
+        /// <param name="limit">The maximum amount of events to return.</param>
+        public FacebookResponse<FacebookFeedCollection> GetEvents(string identifier, int limit) {
+            return GetEvents(identifier, new FacebookFeedOptions {
+                Limit = limit
+            });
+        }
+
+        /// <summary>
+        /// Gets the events of the specified user or page.
+        /// </summary>
+        /// <param name="identifier">The ID or name of the user/page.</param>
+        /// <param name="options">The options for the call to the API.</param>
+        public FacebookResponse<FacebookFeedCollection> GetEvents(string identifier, FacebookFeedOptions options) {
+            return FacebookHelpers.ParseResponse(Raw.GetFeed(identifier, options), FacebookFeedCollection.Parse);
+        }
+
+        #endregion
+
+    }
+
+}
--- src/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
+++ src/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.0.862.2")]
+[assembly: AssemblyFileVersion("0.0.863.1")]
 
