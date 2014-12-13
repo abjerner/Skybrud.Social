2e8be9ab68436d0fb6c43b27a03191d421da41d8
Anders Bjerner <abjerner@skybrud.dk>
2014-12-13 23:50:10 +0100

    Facebook: Updated the documentation for the FacebookEvent class

--- src/Skybrud.Social/Facebook/Objects/FacebookEvent.cs
+++ src/Skybrud.Social/Facebook/Objects/FacebookEvent.cs
@@ -7,18 +7,70 @@ namespace Skybrud.Social.Facebook.Objects {
 
         #region Properties
 
+        /// <summary>
+        /// Gets the ID of the event.
+        /// </summary>
         public string Id { get; private set; }
-        public object Owner { get; private set; }
-        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the cover photo of the event.
+        /// </summary>
+        public FacebookCoverPhoto Cover { get; private set; }
+
+        /// <summary>
+        /// Gets the long-form description of the event.
+        /// </summary>
         public string Description { get; private set; }
-        public DateTime StartTime { get; private set; }
+
+        /// <summary>
+        /// Gets the end time of the event. Not all events have an end time.
+        /// </summary>
         public DateTime? EndTime { get; private set; }
-        public string TimeZone { get; private set; }
+
+        /// <summary>
+        /// Gets whether the event only has a date specified, but no time.
+        /// </summary>
         public bool IsDateOnly { get; private set; }
+
+        /// <summary>
+        /// Gets the location of the event, if any.
+        /// </summary>
         public string Location { get; private set; }
-        public object Venue { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the event.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the profile that created the event.
+        /// </summary>
+        public object Owner { get; private set; }
+
+        /// <summary>
+        /// Gets who can see the event.
+        /// </summary>
         public string Privacy { get; private set; }
+
+        /// <summary>
+        /// Gets the start time of the event.
+        /// </summary>
+        public DateTime StartTime { get; private set; }
+
+        /// <summary>
+        /// Gets the timezone of the event.
+        /// </summary>
+        public string TimeZone { get; private set; }
+
+        /// <summary>
+        /// Gets the last time the event was updated.
+        /// </summary>
         public DateTime UpdatedTime { get; private set; }
+
+        /// <summary>
+        /// Gets the vanue hosting the event, if any.
+        /// </summary>
+        public object Venue { get; private set; }
 
         #endregion
 
@@ -31,15 +83,18 @@ namespace Skybrud.Social.Facebook.Objects {
         public static FacebookEvent Parse(JsonObject obj) {
             return new FacebookEvent(obj) {
                 Id = obj.GetString("id"),
-                Name = obj.GetString("name"),
+                Cover = obj.GetObject("cover", FacebookCoverPhoto.Parse),
                 Description = obj.GetString("description"),
-                StartTime = obj.GetDateTime("start_time"),
-                EndTime = obj.HasValue("end_time") ? (DateTime?) obj.GetDateTime("end_time") : null,
-                TimeZone = obj.GetString("timezone"),
+                EndTime = obj.HasValue("end_time") ? (DateTime?)obj.GetDateTime("end_time") : null,
                 IsDateOnly = obj.HasValue("is_date_only") && obj.GetBoolean("is_date_only"),
                 Location = obj.GetString("location"),
+                Name = obj.GetString("name"),
+                // owner
                 Privacy = obj.GetString("privacy"),
+                StartTime = obj.GetDateTime("start_time"),
+                TimeZone = obj.GetString("timezone"),
                 UpdatedTime = obj.GetDateTime("updated_time")
+                // venue
             };
         }
         
--- src/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
+++ src/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.0.864.7")]
+[assembly: AssemblyFileVersion("0.0.866.1")]
 
