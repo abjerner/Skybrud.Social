fe98d5346b2a3f7e5ad9c37d8d70684a90235703
Anders Bjerner <abjerner@skybrud.dk>
2014-12-22 00:50:04 +0100

    Facebook: Updated and cleaned the "FacebookLink" class
    Seems like the "UpdatedTime" property has been removed from the API making my implementation break. Therefore the property has now been removed from Skybrud.Social as well. Also did some minor cleanup in the class file.

--- src/Skybrud.Social/Facebook/Objects/FacebookLink.cs
+++ src/Skybrud.Social/Facebook/Objects/FacebookLink.cs
@@ -1,5 +1,4 @@
 using System;
-using Skybrud.Social.Facebook.Exceptions;
 using Skybrud.Social.Json;
 
 namespace Skybrud.Social.Facebook.Objects {
@@ -15,7 +14,6 @@ namespace Skybrud.Social.Facebook.Objects {
         public string Caption { get; private set; }
         public string Description { get; private set; }
         public DateTime CreatedTime { get; private set; }
-        public DateTime UpdatedTime { get; private set; }
 
         #endregion
 
@@ -27,23 +25,8 @@ namespace Skybrud.Social.Facebook.Objects {
 
         #region Static methods
 
-        public static FacebookLink LoadJson(string path) {
-            return JsonObject.LoadJson(path, Parse);
-        }
-
-        public static FacebookLink ParseJson(string json) {
-            return JsonObject.ParseJson(json, Parse);
-        }
-
         public static FacebookLink Parse(JsonObject obj) {
-
-            // Return NULL if NULL
             if (obj == null) return null;
-
-            // Some error checking
-            if (obj.HasValue("error")) throw obj.GetObject("error", FacebookApiException.Parse);
-
-            // Initialize the link object
             return new FacebookLink(obj) {
                 Id = obj.GetString("id"),
                 Message = obj.GetString("message"),
@@ -51,10 +34,8 @@ namespace Skybrud.Social.Facebook.Objects {
                 Link = obj.GetString("link"),
                 Caption = obj.GetString("caption"),
                 Description = obj.GetString("description"),
-                CreatedTime = DateTime.Parse(obj.GetString("created_time")),
-                UpdatedTime = DateTime.Parse(obj.GetString("updated_time"))
+                CreatedTime = DateTime.Parse(obj.GetString("created_time"))
             };
-
         }
 
         #endregion
