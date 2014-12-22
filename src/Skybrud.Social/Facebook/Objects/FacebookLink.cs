8201ff38960cd09c317d1dd1b7f6e019cdddb0ff
Anders Bjerner <abjerner@skybrud.dk>
2014-12-22 01:04:58 +0100

    Facebook: Added missing properties to the "FacebookLink" class
    + added documentation for the various properties

--- src/Skybrud.Social/Facebook/Objects/FacebookLink.cs
+++ src/Skybrud.Social/Facebook/Objects/FacebookLink.cs
@@ -7,13 +7,50 @@ namespace Skybrud.Social.Facebook.Objects {
 
         #region Properties
 
+        /// <summary>
+        /// Gets the ID of the link.
+        /// </summary>
         public string Id { get; private set; }
-        public string Message { get; private set; }
-        public string Name { get; private set; }
-        public string Link { get; private set; }
-        public string Caption { get; private set; }
-        public string Description { get; private set; }
+
+        /// <summary>
+        /// Gets the time the message was published.
+        /// </summary>
         public DateTime CreatedTime { get; private set; }
+
+        /// <summary>
+        /// Gets the description of the link (appears beneath the link caption).
+        /// </summary>
+        public string Description { get; private set; }
+
+        /// <summary>
+        /// Gets the user that created the link.
+        /// </summary>
+        public FacebookFrom From { get; private set; }
+
+        /// <summary>
+        /// Gets the URL to the link icon that Facebook displays in the news feed.
+        /// </summary>
+        public string Icon { get; private set; }
+
+        /// <summary>
+        /// Gets the URL that was shared.
+        /// </summary>
+        public string Link { get; private set; }
+
+        /// <summary>
+        /// Gets the optional message from the user about this link.
+        /// </summary>
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// Gets the name of the link.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the URL to the thumbnail image used in the link post.
+        /// </summary>
+        public string Picture { get; private set; }
 
         #endregion
 
@@ -29,12 +66,14 @@ namespace Skybrud.Social.Facebook.Objects {
             if (obj == null) return null;
             return new FacebookLink(obj) {
                 Id = obj.GetString("id"),
+                CreatedTime = DateTime.Parse(obj.GetString("created_time")),
+                Description = obj.GetString("description"),
+                From = obj.GetObject("from", FacebookFrom.Parse),
+                Icon = obj.GetString("icon"),
+                Link = obj.GetString("link"),
                 Message = obj.GetString("message"),
                 Name = obj.GetString("name"),
-                Link = obj.GetString("link"),
-                Caption = obj.GetString("caption"),
-                Description = obj.GetString("description"),
-                CreatedTime = DateTime.Parse(obj.GetString("created_time"))
+                Picture = obj.GetString("picture")
             };
         }
 
