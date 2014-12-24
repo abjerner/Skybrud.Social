4626c506cd964207a00eeb7e34fe7db6efdcb4ef
Anders Bjerner <abjerner@skybrud.dk>
2014-12-24 16:26:44 +0100

    Facebook: Updated the "FacebookUser" class with more properties and property documentation
    There are still some properties that I haven't added, but this covers the most basic properties. See more here: https://developers.facebook.com/docs/graph-api/reference/v2.2/user

--- src/Skybrud.Social/Facebook/Objects/FacebookUser.cs
+++ src/Skybrud.Social/Facebook/Objects/FacebookUser.cs
@@ -1,3 +1,4 @@
+using Skybrud.Social.Facebook.Enums;
 using Skybrud.Social.Facebook.Exceptions;
 using Skybrud.Social.Json;
 
@@ -7,20 +8,111 @@ namespace Skybrud.Social.Facebook.Objects {
 
         #region Properties
 
+        /// <summary>
+        /// Gets the ID of the user. As of v2.0 of the Facebook Graph API, IDs are unique to each app and cannot be
+        /// used across different apps.
+        /// </summary>
         public string Id { get; private set; }
-        public string Name { get; private set; }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public string Link { get; private set; }
-        public string Username { get; private set; }
-        public FacebookObject Hometown { get; private set; }
-        public FacebookObject Location { get; private set; }
-        public string Gender { get; private set; }
+
+        /// <summary>
+        /// Gets the "about me" section of the person's profile.
+        /// </summary>
+        public string About { get; private set; }
+
+        /// <summary>
+        /// Gets the person's bio.
+        /// </summary>
+        public string Bio { get; private set; }
+
+        /// <summary>
+        /// Gets the birthday of the user. The birthday is specified using the <code>MM/DD/YYYY</code> format.
+        /// </summary>
+        public string Birthday { get; private set; }
+
+        /// <summary>
+        /// Gets information about the cover photo of the user.
+        /// </summary>
+        public FacebookCoverPhoto Cover { get; private set; }
+
+        /// <summary>
+        /// Gets the email address of the user.
+        /// </summary>
         public string Email { get; private set; }
-        public int? Timezone { get; private set; }
-        public string Locale { get; private set; }
-        public FacebookObject[] Languages { get; private set; }
+
+        /// <summary>
+        /// Gets the first name of the user.
+        /// </summary>
+        public string FirstName { get; private set; }
+
+        /// <summary>
+        /// Gets the gender of the user.
+        /// </summary>
+        public FacebookGender Gender { get; private set; }
+
+        /// <summary>
+        /// Gets information about the hometown of the user.
+        /// </summary>
+        public FacebookObject Hometown { get; private set; }
+
+        /// <summary>
+        /// People with large numbers of followers can have the authenticity of their identity manually verified by
+        /// Facebook. This field indicates whether the user profile is verified in this way.
+        /// </summary>
         public bool IsVerified { get; private set; }
+
+        /// <summary>
+        /// Gets an array of Facebook Pages representing the languages this person knows.
+        /// </summary>
+        public FacebookObject[] Languages { get; private set; }
+
+        /// <summary>
+        /// Gets the last name of the user.
+        /// </summary>
+        public string LastName { get; private set; }
+
+        /// <summary>
+        /// Gets a link to the profile of the user.
+        /// </summary>
+        public string Link { get; private set; }
+
+        /// <summary>
+        /// Gets the selected locale of the user.
+        /// </summary>
+        public string Locale { get; private set; }
+
+        /// <summary>
+        /// Gets the current location of the user as entered by them on their profile. This is not a check in field.
+        /// </summary>
+        public FacebookObject Location { get; private set; }
+
+        /// <summary>
+        /// Gets the middle name of the user.
+        /// </summary>
+        public string MiddleName { get; private set; }
+
+        /// <summary>
+        /// Gets the full name of the user.
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the selected timezone of the user. The timezone is specified as the offset in hours from UTC.
+        /// </summary>
+        public int Timezone { get; private set; }
+
+        /// <summary>
+        /// Indicates whether the user account has been verified. This is distinct from the <code>is_verified</code>
+        /// field. Someone is considered verified if they take any of the following actions:
+        /// - Register for mobile
+        /// - Confirm their account via SMS
+        /// - Enter a valid credit card
+        /// </summary>
+        public bool Verified { get; private set; }
+
+        /// <summary>
+        /// Gets the URL for the website of the user.
+        /// </summary>
+        public string Website { get; private set; }
 
         #endregion
 
@@ -57,19 +149,25 @@ namespace Skybrud.Social.Facebook.Objects {
             if (obj.HasValue("error")) throw obj.GetObject("error", FacebookApiException.Parse);
             return new FacebookUser(obj) {
                 Id = obj.GetString("id"),
-                Name = obj.GetString("name"),
+                About = obj.GetString("about"),
+                Bio = obj.GetString("bio"),
+                Birthday = obj.GetString("birthday"),
+                Cover = obj.GetObject("cover", FacebookCoverPhoto.Parse),
+                Email = obj.GetString("email"),
                 FirstName = obj.GetString("first_name"),
+                Gender = obj.GetEnum("gender", FacebookGender.Unknown),
+                Hometown = obj.GetObject("hometown", FacebookObject.Parse),
+                IsVerified = obj.GetBoolean("is_verified"),
+                Languages = obj.GetArray("languages", FacebookObject.Parse) ?? new FacebookObject[0],
                 LastName = obj.GetString("last_name"),
                 Link = obj.GetString("link"),
-                Username = obj.GetString("username"),
-                Hometown = obj.GetObject("hometown", FacebookObject.Parse),
-                Location = obj.GetObject("location", FacebookObject.Parse),
-                Gender = obj.GetString("gender"),
-                Email = obj.GetString("email"),
-                Timezone = obj.GetInt32("timezone"),
                 Locale = obj.GetString("locale"),
-                Languages = obj.GetArray("languages", FacebookObject.Parse) ?? new FacebookObject[0],
-                IsVerified = obj.GetBoolean("verified")
+                Location = obj.GetObject("location", FacebookObject.Parse),
+                MiddleName = obj.GetString("middle_name"),
+                Name = obj.GetString("name"),
+                Timezone = obj.GetInt32("timezone"),
+                Verified = obj.GetBoolean("verified"),
+                Website = obj.GetString("website")
             };
         }
 
