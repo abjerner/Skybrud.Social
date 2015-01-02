1f311d26e87f00a3e2e0da3420578bb71ac0a653
Anders Bjerner <abjerner@skybrud.dk>
2015-01-02 22:18:15 +0100

    Introduced interfaces IGetOptions and IPostOptions

---
+++ src/Skybrud.Social/Interfaces/IGetOptions.cs
@@ -0,0 +1,18 @@
+﻿using Skybrud.Social.Http;
+
+namespace Skybrud.Social.Interfaces {
+
+    public interface IGetOptions {
+
+        #region Methods
+
+        /// <summary>
+        /// Gets an instance of <code>SocialQueryString</code> representing the GET parameters.
+        /// </summary>
+        SocialQueryString GetQueryString();
+
+        #endregion
+
+    }
+
+}
---
+++ src/Skybrud.Social/Interfaces/IPostOptions.cs
@@ -0,0 +1,24 @@
+﻿using Skybrud.Social.Http;
+
+namespace Skybrud.Social.Interfaces {
+
+    public interface IPostOptions : IGetOptions {
+
+        #region Properties
+
+        bool IsMultipart { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Gets an instance of <code>SocialPostData</code> representing the POST data.
+        /// </summary>
+        SocialPostData GetPostData();
+
+        #endregion
+
+    }
+
+}
