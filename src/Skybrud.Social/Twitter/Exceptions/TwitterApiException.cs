8d79f4eb4e1eb4e0a2ceb0f386602f5431c16406
Anders Bjerner <abjerner@skybrud.dk>
2015-01-26 23:34:11 +0100

    Twitter: Introduced classes TwitterResponse, TwitterResponse<T> and TwitterApiException
    Similar to Facebook and Instagram, I have now implemented response classes and related logic for the Twitter API.

---
+++ src/Skybrud.Social/Twitter/Exceptions/TwitterApiException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Skybrud.Social.Http;
+
+namespace Skybrud.Social.Twitter.Exceptions {
+
+    public class TwitterApiException : Exception {
+
+        #region Properties
+
+        public SocialHttpResponse Response { get; private set; }
+
+        public int Code { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        internal TwitterApiException(SocialHttpResponse response, string message, int code) : base(message) {
+            Response = response;
+            Code = code;
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/Skybrud.Social/Twitter/Responses/TwitterResponse.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using Skybrud.Social.Http;
+using Skybrud.Social.Json;
+using Skybrud.Social.Twitter.Exceptions;
+
+namespace Skybrud.Social.Twitter.Responses {
+
+    public class TwitterResponse {
+
+        /// <summary>
+        /// Gets a reference to the underlying response.
+        /// </summary>
+        public SocialHttpResponse Response { get; private set; }
+
+        protected TwitterResponse(SocialHttpResponse response) {
+            Response = response;
+        }
+
+        public static void ValidateResponse(SocialHttpResponse response, JsonObject obj) {
+
+            // Skip error checking if the server responds with an OK status code
+            if (response.StatusCode == HttpStatusCode.OK) return;
+
+            // Get the "errors" array
+            JsonArray errors = obj.GetArray("errors");
+
+            // Get the first error (don't remember ever seeing multiple errors in the same response)
+            JsonObject error = errors.GetObject(0);
+
+            // Throw the exception
+            throw new TwitterApiException(response, error.GetString("message"), error.GetInt32("code"));
+
+        }
+
+    }
+
+    public class TwitterResponse<T> : TwitterResponse {
+
+        public T Body { get; protected set; }
+
+        protected TwitterResponse(SocialHttpResponse response) : base(response) { }
+
+    }
+
+}
