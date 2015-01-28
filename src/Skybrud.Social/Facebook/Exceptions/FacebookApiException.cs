f4ebfb83a15a7f1c6a110f010f8f4d99c2c56255
Anders Bjerner <abjerner@skybrud.dk>
2015-01-28 23:10:06 +0100

    Facebook: Added "Response" property to class "FacebookApiException"
    Objects representing a successful request all have a "Response"  property returning an instance of "SocialHttpResponse". I have now added a similar property to "FacebookApiResponse" for when a request fails.

--- src/Skybrud.Social/Facebook/Exceptions/FacebookApiException.cs
+++ src/Skybrud.Social/Facebook/Exceptions/FacebookApiException.cs
@@ -1,15 +1,19 @@
 using System;
+using Skybrud.Social.Http;
 using Skybrud.Social.Json;
 
 namespace Skybrud.Social.Facebook.Exceptions {
 
     public class FacebookApiException : Exception {
 
+        public SocialHttpResponse Response { get; private set; }
+
         public int Code { get; private set; }
         public string Type { get; private set; }
         public int Subcode { get; private set; }
 
-        public FacebookApiException(int code, string type, string message, int subcode = 0) : base(message) {
+        public FacebookApiException(SocialHttpResponse response, int code, string type, string message, int subcode = 0) : base(message) {
+            Response = response;
             Code = code;
             Type = type;
             Subcode = subcode;
@@ -19,6 +23,7 @@ namespace Skybrud.Social.Facebook.Exceptions {
         public static FacebookApiException Parse(JsonObject obj) {
 
             return new FacebookApiException(
+                null,
                 obj.GetInt32("code"),
                 obj.GetString("type"),
                 obj.GetString("message"),
--- src/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
+++ src/Skybrud.Social/Properties/AssemblyInfoGenerated.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.0.911.1")]
+[assembly: AssemblyFileVersion("0.0.912.2")]
 
