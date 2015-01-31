2b25c3fe67928a356288270ce7e04e7abdb7b46d
Anders Bjerner <abjerner@skybrud.dk>
2015-01-31 23:48:17 +0100

    Improved code comments of the "FacebookException" and "InstagramException" classes

--- src/Skybrud.Social/Facebook/Exceptions/FacebookException.cs
+++ src/Skybrud.Social/Facebook/Exceptions/FacebookException.cs
@@ -5,11 +5,32 @@ namespace Skybrud.Social.Facebook.Exceptions {
 
     public class FacebookException : Exception {
 
+        #region Properties
+
+        /// <summary>
+        /// Gets a reference to the underlying <code>SocialHttpResponse</code>.
+        /// </summary>
         public SocialHttpResponse Response { get; private set; }
 
+        /// <summary>
+        /// Gets the error code received from the Facebook API.
+        /// </summary>
         public int Code { get; private set; }
+
+        /// <summary>
+        /// Gets the type of the error received from the Facebook API.
+        /// </summary>
         public string Type { get; private set; }
+
+        /// <summary>
+        /// Gets the sub error code received from the Facebook API. If a sub error code isn't specified in the
+        /// response, <code>0</code> will be returned.
+        /// </summary>
         public int Subcode { get; private set; }
+
+        #endregion
+
+        #region Constructors
 
         public FacebookException(SocialHttpResponse response, int code, string type, string message, int subcode = 0) : base(message) {
             Response = response;
@@ -17,6 +38,8 @@ namespace Skybrud.Social.Facebook.Exceptions {
             Type = type;
             Subcode = subcode;
         }
+
+        #endregion
 
     }
 
--- src/Skybrud.Social/Instagram/Exceptions/InstagramException.cs
+++ src/Skybrud.Social/Instagram/Exceptions/InstagramException.cs
@@ -5,15 +5,34 @@ namespace Skybrud.Social.Instagram.Exceptions {
 
     public class InstagramException : Exception {
 
+        #region Properties
+
+        /// <summary>
+        /// Gets a reference to the underlying <code>SocialHttpResponse</code>.
+        /// </summary>
         public SocialHttpResponse Response { get; private set; }
+
+        /// <summary>
+        /// Gets the error code received from the Instagram API.
+        /// </summary>
         public int Code { get; private set; }
+
+        /// <summary>
+        /// Gets the type of the error received from the Instagram API.
+        /// </summary>
         public string Type { get; private set; }
+
+        #endregion
+
+        #region Constructors
 
         internal InstagramException(SocialHttpResponse response, int code, string type, string message) : base(message) {
             Response = response;
             Code = code;
             Type = type;
         }
+
+        #endregion
 
     }
 
