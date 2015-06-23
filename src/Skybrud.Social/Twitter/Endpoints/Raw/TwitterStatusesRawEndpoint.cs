841c7f0e31eac13864ec92a132eb04ebad45e4b6
Anders Bjerner <abjerner@skybrud.dk>
2015-06-23 23:46:53 +0200

    Twitter: Implemented methods for retweeting and deleting tweets

--- src/Skybrud.Social/Twitter/Endpoints/Raw/TwitterStatusesRawEndpoint.cs
+++ src/Skybrud.Social/Twitter/Endpoints/Raw/TwitterStatusesRawEndpoint.cs
@@ -209,6 +209,50 @@ namespace Skybrud.Social.Twitter.Endpoints.Raw {
             return Client.DoHttpGetRequest("https://api.twitter.com/1.1/statuses/retweets_of_me.json", options);
         }
 
+        /// <summary>
+        /// Retweets the tweet with the specified <code>id</code>.
+        /// </summary>
+        /// <param name="id">The ID of the tweet to be retweeted.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public SocialHttpResponse Retweet(long id) {
+            return Retweet(id, false);
+        }
+
+        /// <summary>
+        /// Retweets the tweet with the specified <code>id</code>.
+        /// </summary>
+        /// <param name="id">The ID of the tweet to be retweeted.</param>
+        /// <param name="trimUser">When set to <code>true</code>, each tweet returned in a timeline will include a user
+        /// object including only the status authors numerical ID. Omit this parameter to receive the complete user
+        /// object.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public SocialHttpResponse Retweet(long id, bool trimUser) {
+            return Client.DoHttpPostRequest("https://api.twitter.com/1.1/statuses/retweet/" + id + ".json" + (trimUser ? "?trim_user=true" : ""), null);
+        }
+
+        /// <summary>
+        /// Destroys the status with the specified <code>id</code>. The authenticating user must be the author of the
+        /// specified status. Returns the destroyed status if successful.
+        /// </summary>
+        /// <param name="id">The ID of the tweet to be destroyed.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public SocialHttpResponse DestroyStatusMessage(long id) {
+            return DestroyStatusMessage(id, false);
+        }
+        
+        /// <summary>
+        /// Destroys the status with the specified <code>id</code>. The authenticating user must be the author of the
+        /// specified status. Returns the destroyed status if successful.
+        /// </summary>
+        /// <param name="id">The ID of the tweet to be destroyed.</param>
+        /// <param name="trimUser">When set to <code>true</code>, each tweet returned in a timeline will include a user
+        /// object including only the status authors numerical ID. Omit this parameter to receive the complete user
+        /// object.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public SocialHttpResponse DestroyStatusMessage(long id, bool trimUser) {
+            return Client.DoHttpPostRequest("https://api.twitter.com/1.1/statuses/destroy/" + id + ".json" + (trimUser ? "?trim_user=true" : ""), null);
+        }
+
         #endregion
 
     }
--- src/Skybrud.Social/Twitter/Endpoints/TwitterStatusesEndpoint.cs
+++ src/Skybrud.Social/Twitter/Endpoints/TwitterStatusesEndpoint.cs
@@ -1,4 +1,7 @@
+using System;
+using Skybrud.Social.Http;
 using Skybrud.Social.Twitter.Endpoints.Raw;
+using Skybrud.Social.Twitter.Objects;
 using Skybrud.Social.Twitter.Options;
 using Skybrud.Social.Twitter.Responses;
 
@@ -185,6 +188,98 @@ namespace Skybrud.Social.Twitter.Endpoints {
         /// <param name="options">The options for the call.</param>
         public TwitterTimelineResponse GetRetweetsOfMe(TwitterTimelineOptions options) {
             return TwitterTimelineResponse.ParseResponse(Raw.GetRetweetsOfMe(options));
+        }
+
+        /// <summary>
+        /// Retweets the status with the specified <code>id</code>.
+        /// </summary>
+        /// <param name="id">The ID of the tweet to be retweeted.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public TwitterStatusMessageResponse Retweet(long id) {
+            return Retweet(id, false);
+        }
+
+        /// <summary>
+        /// Retweets the status with the specified <code>id</code>.
+        /// </summary>
+        /// <param name="id">The ID of the tweet to be retweeted.</param>
+        /// <param name="trimUser">When set to <code>true</code>, each tweet returned in a timeline will include a user
+        /// object including only the status authors numerical ID. Omit this parameter to receive the complete user
+        /// object.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public TwitterStatusMessageResponse Retweet(long id, bool trimUser) {
+            return TwitterStatusMessageResponse.ParseResponse(Raw.Retweet(id, trimUser));
+        }
+
+        /// <summary>
+        /// Retweets the specified <code>status</code>.
+        /// </summary>
+        /// <param name="status">The tweet to be retweeted.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public TwitterStatusMessageResponse Retweet(TwitterStatusMessage status) {
+            if (status == null) throw new ArgumentNullException("status");
+            return Retweet(status.Id, false);
+        }
+
+        /// <summary>
+        /// Retweets the specified <code>status</code>.
+        /// </summary>
+        /// <param name="status">The status to be retweeted.</param>
+        /// <param name="trimUser">When set to <code>true</code>, each tweet returned in a timeline will include a user
+        /// object including only the status authors numerical ID. Omit this parameter to receive the complete user
+        /// object.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public TwitterStatusMessageResponse Retweet(TwitterStatusMessage status, bool trimUser) {
+            if (status == null) throw new ArgumentNullException("status");
+            return Retweet(status.Id, trimUser);
+        }
+
+        /// <summary>
+        /// Destroys the status with the specified <code>id</code>. The authenticating user must be the author of the
+        /// specified status. Returns the destroyed status if successful.
+        /// </summary>
+        /// <param name="id">The ID of the status to be destroyed.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public TwitterStatusMessageResponse DestroyStatusMessage(long id) {
+            return DestroyStatusMessage(id, false);
+        }
+
+        /// <summary>
+        /// Destroys the status with the specified <code>id</code>. The authenticating user must be the author of the
+        /// specified status. Returns the destroyed status if successful.
+        /// </summary>
+        /// <param name="id">The ID of the status to be destroyed.</param>
+        /// <param name="trimUser">When set to <code>true</code>, each tweet returned in a timeline will include a user
+        /// object including only the status authors numerical ID. Omit this parameter to receive the complete user
+        /// object.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public TwitterStatusMessageResponse DestroyStatusMessage(long id, bool trimUser) {
+            return TwitterStatusMessageResponse.ParseResponse(Raw.DestroyStatusMessage(id, trimUser));
+        }
+
+        /// <summary>
+        /// Destroys the specified <code>status</code>. The authenticating user must be the author of the
+        /// specified status. Returns the destroyed status if successful.
+        /// </summary>
+        /// <param name="status">The status to be destroyed.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public TwitterStatusMessageResponse DestroyStatusMessage(TwitterStatusMessage status) {
+            if (status == null) throw new ArgumentNullException("status");
+            return DestroyStatusMessage(status.Id, false);
+        }
+
+        /// <summary>
+        /// Destroys the status with the specified <code>id</code>. The authenticating user must be the author of the
+        /// specified status. Returns the destroyed status if successful.
+        /// </summary>
+        /// <param name="status">The status to be destroyed.</param>
+        /// <param name="trimUser">When set to <code>true</code>, each tweet returned in a timeline will include a user
+        /// object including only the status authors numerical ID. Omit this parameter to receive the complete user
+        /// object.</param>
+        /// <returns>Returns the response from the API.</returns>
+        public TwitterStatusMessageResponse DestroyStatusMessage(TwitterStatusMessage status, bool trimUser) {
+            if (status == null) throw new ArgumentNullException("status");
+            return DestroyStatusMessage(status.Id, trimUser);
         }
 
         #endregion
