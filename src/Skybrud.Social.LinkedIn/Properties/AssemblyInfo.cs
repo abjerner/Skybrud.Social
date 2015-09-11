00ba85ab6cb70e59dd8d8a3fc2bbcbb140dc8644
Anders Bjerner <abjerner@skybrud.dk>
2015-09-12 01:44:08 +0200

    LinkedIn: Just some build stuff

--- src/Skybrud.Social.LinkedIn/Properties/AssemblyInfo.cs
+++ src/Skybrud.Social.LinkedIn/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -8,8 +7,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Skybrud.Social.LinkedIn")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Skybrud.Social.LinkedIn")]
+[assembly: AssemblyCompany("Skybrud.dk")]
+[assembly: AssemblyProduct("Skybrud.Social")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyInformationalVersion("0.9.0.0")]
