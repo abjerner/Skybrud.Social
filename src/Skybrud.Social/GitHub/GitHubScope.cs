979de21b7982421674bfebc44cfb58c63d6067dc
Anders Bjerner <abjerner@skybrud.dk>
2015-12-26 23:37:01 +0100

    GitHub: Improved the code involving scope

--- src/Skybrud.Social/GitHub/GitHubScope.cs
+++ src/Skybrud.Social/GitHub/GitHubScope.cs
@@ -1,6 +1,14 @@
+using System;
+using Skybrud.Social.GitHub.Scopes;
+
 namespace Skybrud.Social.GitHub {
 
+    /// <summary>
+    /// Class representing a scope of the GitHub API.
+    /// </summary>
     public class GitHubScope {
+
+        // TODO: Move to the "Skybrud.Social.GitHub.Scopes" namespace for v1.0
 
         #region Constants
         
@@ -8,104 +16,67 @@ namespace Skybrud.Social.GitHub {
         /// Grants read-only access to public information (includes public user profile info,
         /// public repository info, and gists).
         /// </summary>
-        public static readonly GitHubScope Default = new GitHubScope(
-            "",
-            "Grants read-only access to public information (includes public user profile info, " +
-            "public repository info, and gists)."
-        );
+        [Obsolete("Use GitHubScopes.Default instead.")]
+        public static readonly GitHubScope Default = GitHubScopes.Default;
 
         /// <summary>
         /// Grants read/write access to profile info only. Note that this scope includes
         /// <code>user:email</code> and <code>user:follow</code>.
         /// </summary>
-        public static readonly GitHubScope User = new GitHubScope(
-            "user",
-            "Grants read/write access to profile info only. Note that this scope includes user:email and user:follow."
-        );
+        [Obsolete("Use GitHubScopes.User instead.")]
+        public static readonly GitHubScope User = GitHubScopes.User;
 
         /// <summary>
         /// Grants read access to a user’s email addresses.
         /// </summary>
-        public static readonly GitHubScope UserEmail = new GitHubScope(
-            "user:email",
-            "Grants read access to a user’s email addresses."
-        );
+        [Obsolete("Use GitHubScopes.UserEmail instead.")]
+        public static readonly GitHubScope UserEmail = GitHubScopes.UserEmail;
 
         /// <summary>
         /// Grants access to follow or unfollow other users.
         /// </summary>
-        public static readonly GitHubScope UserFollow = new GitHubScope(
-            "user:follow",
-            "Grants access to follow or unfollow other users."
-        );
+        [Obsolete("Use GitHubScopes.UserFollow instead.")]
+        public static readonly GitHubScope UserFollow = GitHubScopes.UserFollow;
 
         /// <summary>
         /// Grants read/write access to code, commit statuses, and deployment statuses for public
         /// repositories and organizations.
         /// </summary>
-        public static readonly GitHubScope PublicRepo = new GitHubScope(
-            "public_repo",
-            "Grants read/write access to code, commit statuses, and deployment statuses for " +
-            "public repositories and organizations."
-        );
+        [Obsolete("Use GitHubScopes.PublicRepo instead.")]
+        public static readonly GitHubScope PublicRepo = GitHubScopes.PublicRepo;
 
         /// <summary>
         /// Grants read/write access to code, commit statuses, and deployment statuses for public
         /// and private repositories and organizations.
         /// </summary>
-        public static readonly GitHubScope Repo = new GitHubScope(
-            "repo",
-            "Grants read/write access to code, commit statuses, and deployment statuses for " +
-            "public and private repositories and organizations."
-        );
-
-        // TODO: Add scope "repo_deployment"
+        [Obsolete("Use GitHubScopes.Repo instead.")]
+        public static readonly GitHubScope Repo = GitHubScopes.Repo;
 
         /// <summary>
         /// Grants read/write access to public and private repository commit statuses. This scope
         /// is only necessary to grant other users or services access to private repository commit
         /// statuses without granting access to the code.
         /// </summary>
-        public static readonly GitHubScope RepoStatus = new GitHubScope(
-            "repo:status",
-            "Grants read/write access to public and private repository commit statuses. This " +
-            "scope is only necessary to grant other users or services access to private " +
-            "repository commit statuses without granting access to the code."
-        );
+        [Obsolete("Use GitHubScopes.RepoStatus instead.")]
+        public static readonly GitHubScope RepoStatus = GitHubScopes.RepoStatus;
 
         /// <summary>
         /// Grants access to delete adminable repositories.
         /// </summary>
-        public static readonly GitHubScope DeleteRepo = new GitHubScope(
-            "delete_repo",
-            "Grants access to delete adminable repositories."
-        );
+        [Obsolete("Use GitHubScopes.DeleteRepo instead.")]
+        public static readonly GitHubScope DeleteRepo = GitHubScopes.DeleteRepo;
 
         /// <summary>
         /// Grants read access to a user’s notifications. <code>repo</code> also provides this access.
         /// </summary>
-        public static readonly GitHubScope Notifications = new GitHubScope(
-            "notifications",
-            "Grants read access to a user’s notifications. repo also provides this access."
-        );
+        [Obsolete("Use GitHubScopes.Notifications instead.")]
+        public static readonly GitHubScope Notifications = GitHubScopes.Notifications;
 
         /// <summary>
         /// Grants write access to gists.
         /// </summary>
-        public static readonly GitHubScope Gist = new GitHubScope(
-            "gist",
-            "Grants write access to gists."
-        );
-
-        // TODO: Add scope "read:repo_hook"
-        // TODO: Add scope "write:repo_hook"
-        // TODO: Add scope "admin:repo_hook"
-        // TODO: Add scope "read:org"
-        // TODO: Add scope "write:org"
-        // TODO: Add scope "admin:org"
-        // TODO: Add scope "read:public_key"
-        // TODO: Add scope "write:public_key"
-        // TODO: Add scope "admin:public_key"
+        [Obsolete("Use GitHubScopes.Gist instead.")]
+        public static readonly GitHubScope Gist = GitHubScopes.Gist;
 
         #endregion
 
--- src/Skybrud.Social/GitHub/GitHubScopeCollection.cs
+++ src/Skybrud.Social/GitHub/GitHubScopeCollection.cs
@@ -4,7 +4,12 @@ using System.Linq;
 
 namespace Skybrud.Social.GitHub {
 
+    /// <summary>
+    /// Class representing a collection of scopes of the GitHub API.
+    /// </summary>
     public class GitHubScopeCollection {
+
+        // TODO: Move to the "Skybrud.Social.GitHub.Scopes" namespace for v1.0
 
         #region Private fields
 
@@ -14,6 +19,9 @@ namespace Skybrud.Social.GitHub {
 
         #region Properties
 
+        /// <summary>
+        /// Gets an array of all scopes added to the collection.
+        /// </summary>
         public GitHubScope[] Items {
             get { return _list.ToArray(); }
         }
@@ -22,6 +30,10 @@ namespace Skybrud.Social.GitHub {
 
         #region Constructors
 
+        /// <summary>
+        /// Initializes a new collection with the specified array of <code>scopes</code>.
+        /// </summary>
+        /// <param name="scopes">The array of scopes the collection should be based on.</param>
         public GitHubScopeCollection(params GitHubScope[] scopes) {
             _list.AddRange(scopes);
         }
@@ -30,18 +42,35 @@ namespace Skybrud.Social.GitHub {
 
         #region Member methods
 
+        /// <summary>
+        /// Adds the specified <code>scope</code> to the collection.
+        /// </summary>
+        /// <param name="scope">The scope to be added.</param>
         public void Add(GitHubScope scope) {
             _list.Add(scope);
         }
 
+        /// <summary>
+        /// Converts the collection of scopes into an array of <code>GitHubScope</code>.
+        /// </summary>
+        /// <returns>Returns an array of <code>GitHubScope</code>.</returns>
         public GitHubScope[] ToArray() {
             return _list.ToArray();
         }
 
+        /// <summary>
+        /// Returns a string array representing the collection.
+        /// </summary>
+        /// <returns>Returns a <code>string</code> array.</returns>
         public string[] ToStringArray() {
             return (from scope in _list where scope.Name.Length > 0 select scope.Name).ToArray();
         }
 
+        /// <summary>
+        /// Returns a string representation of the collection. The string will consist of the names of scopes added to
+        /// the collection, each separated by a comma.
+        /// </summary>
+        /// <returns>Returns a string representation of the collection.</returns>
         public override string ToString() {
             return String.Join(",", ToStringArray());
         }
@@ -50,13 +79,33 @@ namespace Skybrud.Social.GitHub {
 
         #region Operator overloading
 
+        /// <summary>
+        /// Initializes a new collection based on a single <code>scope</code>.
+        /// </summary>
+        /// <param name="scope">The scope the collection should be based on.</param>
+        /// <returns>Returns a new collection based on a single <code>scope</code>.</returns>
+        public static implicit operator GitHubScopeCollection(GitHubScope scope) {
+            return new GitHubScopeCollection(scope);
+        }
+
+        /// <summary>
+        /// Initializes a new collection based on the specified <code>array</code> of scopes.
+        /// </summary>
+        /// <param name="array">The array the collection should be based on.</param>
+        /// <returns>Returns a new instance of <code>GitHubScopeCollection</code>.</returns>
         public static implicit operator GitHubScopeCollection(GitHubScope[] array) {
             return new GitHubScopeCollection(array ?? new GitHubScope[0]);
         }
 
-        public static GitHubScopeCollection operator +(GitHubScopeCollection left, GitHubScope right) {
-            left.Add(right);
-            return left;
+        /// <summary>
+        /// Adds the specified <code>scope</code> to <code>collection</code>.
+        /// </summary>
+        /// <param name="collection">The collection.</param>
+        /// <param name="scope">The scope to be added.</param>
+        /// <returns>Returns the collection.</returns>
+        public static GitHubScopeCollection operator +(GitHubScopeCollection collection, GitHubScope scope) {
+            collection.Add(scope);
+            return collection;
         }
 
         #endregion
