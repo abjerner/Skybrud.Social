78817a78850a502f42df096f7bff0df8adc227ee
Anders Bjerner <abjerner@skybrud.dk>
2015-12-31 20:25:36 +0100

    Introduced the "PropertyNotSetException" class
    Now the class is also in the repository.

---
+++ src/Skybrud.Social/Exceptions/PropertyNotSetException.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Skybrud.Social.Exceptions {
+    
+    /// <summary>
+    /// Class representing an exception for a property that is not set.
+    /// </summary>
+    public class PropertyNotSetException : Exception {
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the name of the property.
+        /// </summary>
+        public string PropertyName { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new exception for the property with the specified <code>propertyName</code>.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        public PropertyNotSetException(string propertyName) : this(propertyName, "Property cannot be empty.") { }
+
+        /// <summary>
+        /// Initializes a new exception for the property with the specified <code>propertyName</code>.
+        /// </summary>
+        /// <param name="propertyName">The name of the property.</param>
+        /// <param name="message">The message of the exception.</param>
+        public PropertyNotSetException(string propertyName, string message) : base(message) {
+            PropertyName = propertyName;
+        }
+
+        #endregion
+
+        #region Member methods
+
+        public override string Message {
+            get {
+                String s = base.Message;
+                if (!String.IsNullOrEmpty(PropertyName)) {
+                    return s + Environment.NewLine + "Property name: " + PropertyName;
+                }
+                return s;
+            }
+        }
+
+        #endregion
+
+    }
+
+}
