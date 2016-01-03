71203a49f2dcc3078b2be45a6b2e3a9efc0b10f5
Anders Bjerner <abjerner@skybrud.dk>
2016-01-03 14:34:58 +0100

    It's 2016 :D

--- src/Skybrud.Social/Properties/AssemblyInfo.cs
+++ src/Skybrud.Social/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using Skybrud.Social.Attributes;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Skybrud.dk a/s")]
 [assembly: AssemblyProduct("Skybrud.Social")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
