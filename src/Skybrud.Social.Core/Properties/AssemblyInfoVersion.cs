1d1c75d848ee960169e74e099e56c4ce5b2765ae
Anders Bjerner <abjerner@skybrud.dk>
2016-03-06 15:14:28 +0100

    First beta release of Skybrud.Social.Core :D

--- src/Skybrud.Social.Core/Properties/AssemblyInfoVersion.cs
+++ src/Skybrud.Social.Core/Properties/AssemblyInfoVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-alpha5")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta1")]
 [assembly: AssemblyMetadata("StartDate", "2012-07-30")]
