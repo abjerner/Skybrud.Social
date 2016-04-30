dd4f1034984105924f40d1799fed8d40776744c5
Anders Bjerner <abjerner@skybrud.dk>
2016-04-30 22:53:19 +0200

    Json: The GetFloat methods now uses JPath instead of the property name

--- src/Skybrud.Social.Core/Json/Extensions/JObjectExtensions.cs
+++ src/Skybrud.Social.Core/Json/Extensions/JObjectExtensions.cs
@@ -150,26 +150,32 @@ namespace Skybrud.Social.Json.Extensions {
         }
 
         /// <summary>
-        /// Gets a float from a property with the specified <code>propertyName</code>.
+        /// Gets the <see cref="System.Single"/> value of the token matching the specified <code>path</code>, or
+        /// <code>0</code> if <code>path</code> doesn't match a token.
         /// </summary>
-        /// <param name="obj">The parent object of the property.</param>
-        /// <param name="propertyName">The name of the property.</param>
-        public static float GetFloat(this Newtonsoft.Json.Linq.JObject obj, string propertyName) {
-            if (obj == null || !obj.HasValue(propertyName)) return default(float);
-            JToken property = obj.GetValue(propertyName);
-            return property == null ? default(float) : property.Value<float>();
+        /// <param name="obj">The parent object.</param>
+        /// <param name="path">A <see cref="String"/> that contains a JPath expression.</param>
+        /// <returns>Returns an instance of <see cref="System.Int64"/>.</returns>
+        public static float GetFloat(this JObject obj, string path) {
+            if (obj == null) return default(long);
+            JToken token = obj.SelectToken(path);
+            return token == null || token.Type == JTokenType.Null ? default(float) : token.Value<float>();
         }
 
         /// <summary>
-        /// Gets a float from a property with the specified <code>propertyName</code>.
+        /// Gets the <see cref="System.Single"/> value of the token matching the specified <code>path</code> and parses
+        /// it into an instance of <code>T</code>, or the default value of <code>T</code> if <code>path</code> doesn't
+        /// match a token.
         /// </summary>
-        /// <param name="obj">The parent object of the property.</param>
-        /// <param name="propertyName">The name of the property.</param>
-        /// <param name="func">A callback function used for parsing or converting the property value.</param>
-        public static T GetFloat<T>(this Newtonsoft.Json.Linq.JObject obj, string propertyName, Func<float, T> func) {
-            if (obj == null || !obj.HasValue(propertyName)) return default(T);
-            JToken property = obj.GetValue(propertyName);
-            return property == null ? default(T) : func(property.Value<float>());
+        /// <param name="obj">The parent object.</param>
+        /// <param name="path">A <see cref="String"/> that contains a JPath expression.</param>
+        /// <param name="callback">A callback function used for parsing or converting the property value.</param>
+        /// <returns>Returns an instance of <see cref="System.Single"/>, or <code>0</code> if <code>path</code> doesn't
+        /// match a token.</returns>
+        public static T GetFloat<T>(this JObject obj, string path, Func<long, T> callback) {
+            if (obj == null) return default(T);
+            JToken token = obj.SelectToken(path);
+            return token == null || token.Type == JTokenType.Null ? default(T) : callback(token.Value<long>());
         }
 
         /// <summary>
