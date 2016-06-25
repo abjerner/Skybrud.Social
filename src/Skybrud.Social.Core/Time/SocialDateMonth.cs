af4e5bdac36a39b5264bd9acb85ad0bbe1f479d4
Anders Bjerner <abjerner@skybrud.dk>
2016-06-25 21:15:26 +0200

    Added "SocialDateMonth" enum class with values representing each month of year

---
+++ src/Skybrud.Social.Core/Time/SocialDateMonth.cs
@@ -0,0 +1,25 @@
+﻿namespace Skybrud.Social.Time {
+
+    // ReSharper disable CSharpWarnings::CS1591
+
+    /// <summary>
+    /// Enum class with values representing each month of a year.
+    /// </summary>
+    public enum SocialDateMonth {
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        October = 10,
+        November = 11,
+        December = 12
+    }
+    
+    // ReSharper restore CSharpWarnings::CS1591
+
+}
