ac240c756c6dfdca8af67984d7574ec800bb11d9
Anders Bjerner <abjerner@skybrud.dk>
2016-07-02 13:28:47 +0200

    Improved XML documentation and a little cleanup

--- src/Skybrud.Social.Core/Json/Converters/SocialDateTimeUnixTimeConverter.cs
+++ src/Skybrud.Social.Core/Json/Converters/SocialDateTimeUnixTimeConverter.cs
@@ -9,12 +9,26 @@ namespace Skybrud.Social.Json.Converters {
     /// </summary>
     public class SocialDateTimeUnixTimeConverter : JsonConverter {
 
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="Newtonsoft.Json.JsonWriter"/> to write to.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="serializer">The calling serializer.</param>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) {
             if (!(value is SocialDateTime)) return;
             SocialDateTime dt = (SocialDateTime) value;
             writer.WriteValue(dt.UnixTimestamp);
         }
 
+        /// <summary>
+        /// Reads the JSON representation of the object.
+        /// </summary>
+        /// <param name="reader">The <see cref="Newtonsoft.Json.JsonReader"/> to read from.</param>
+        /// <param name="objectType">Type of the object.</param>
+        /// <param name="existingValue">The existing value of object being read.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        /// <returns>The object value.</returns>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) {
 
             switch (reader.TokenType) {
@@ -40,6 +54,11 @@ namespace Skybrud.Social.Json.Converters {
 
         }
 
+        /// <summary>
+        /// Determines whether this instance can convert the specified object type.
+        /// </summary>
+        /// <param name="objectType">Type of the object.</param>
+        /// <returns>Returns <code>true</code> if this instance can convert the specified object type; otherwise, <code>false</code>.</returns>
         public override bool CanConvert(Type objectType) {
             return objectType == typeof(SocialDateTime);
         }
