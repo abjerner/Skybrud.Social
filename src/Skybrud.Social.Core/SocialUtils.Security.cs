3113c336295f76b08964e2e3752d9ea0b760d109
Anders Bjerner <abjerner@skybrud.dk>
2016-08-27 14:41:24 +0200

    Added a new security helper class (currently with methods for working with BASE64)
    The methods are needed for the Vimeo implementation, but might as well be a part of the core

---
+++ src/Skybrud.Social.Core/SocialUtils.Security.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Skybrud.Social {
+    
+    public static partial class SocialUtils {
+
+        /// <summary>
+        /// Static class with miscellaneous helper methods.
+        /// </summary>
+        public static class Security {
+
+			/// <summary>
+			/// Base64 encodes the specified <code>input</code> string.
+			/// </summary>
+			/// <param name="input">The input string to be encoded.</param>
+			/// <returns>Returns the Base64 encoded string.</returns>
+			public static string Base64Encode(string input) {
+				return Convert.ToBase64String(Encoding.UTF8.GetBytes(input));
+			}
+
+			/// <summary>
+			/// Base64 decodes the specified <code>input</code> string.
+			/// </summary>
+			/// <param name="input">The input string to be decoded.</param>
+			/// <returns>Returns the Base64 decoded string.</returns>
+			public static string Base64Decode(string input) {
+				return Encoding.UTF8.GetString(Convert.FromBase64String(input));
+			}
+		
+        }
+    
+    }
+
+}
