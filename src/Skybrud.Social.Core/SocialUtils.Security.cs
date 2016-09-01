973debc505b27616554c5105c4d7182f3ec385d4
Anders Bjerner <abjerner@skybrud.dk>
2016-09-01 21:36:15 +0200

    Utils: Added helper methods for basic hashing methods

--- src/Skybrud.Social.Core/SocialUtils.Security.cs
+++ src/Skybrud.Social.Core/SocialUtils.Security.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Skybrud.Social {
@@ -27,7 +30,160 @@ namespace Skybrud.Social {
 			public static string Base64Decode(string input) {
 				return Encoding.UTF8.GetString(Convert.FromBase64String(input));
 			}
-		
+
+            /// <summary>
+            /// Gets the MD5 hash of <code>input</code>, and returns that hash. The input string is converted
+            /// using <see cref="Encoding.UTF8"/>.
+            /// </summary>
+            /// <param name="input">The input string</param>
+            /// <returns>Returns the hash as a 32-character hexadecimal number.</returns>
+            public static string GetMd5Hash(string input) {
+                using (MD5 md5 = MD5.Create()) return GetHash(md5, input);
+            }
+
+            /// <summary>
+            /// Gets the MD5 hash of <code>input</code>, and returns that hash. The input string is converted
+            /// using <code>encoding</code>.
+            /// </summary>
+            /// <param name="input">The input string</param>
+            /// <param name="encoding">The encoding to be used for the conversion.</param>
+            /// <returns>Returns the hash as a 32-character hexadecimal number.</returns>
+            public static string GetMd5Hash(string input, Encoding encoding) {
+                using (MD5 md5 = MD5.Create()) return GetHash(md5, input, encoding);
+            }
+
+            /// <summary>
+            /// Gets the MD5 hash of the file specified by the <code>path</code>. The hash is a 64-character
+            /// hexadecimal number.
+            /// </summary>
+            /// <param name="path">The path of the file.</param>
+            /// <returns>Returns the hash as a 32-character hexadecimal number.</returns>
+            public static string GetMd5HashFromFile(string path) {
+                using (MD5 md5 = MD5.Create()) return GetHashFromFile(md5, path);
+            }
+
+            /// <summary>
+            /// Gets the SHA1 hash of <code>input</code>, and returns that hash. The input string is converted
+            /// using <see cref="Encoding.UTF8"/>.
+            /// </summary>
+            /// <param name="input">The input string</param>
+            /// <returns>Returns the hash as a 40-character hexadecimal number.</returns>
+            public static string GetSha1Hash(string input) {
+                using (SHA1 sha1 = SHA1.Create()) return GetHash(sha1, input);
+            }
+
+            /// <summary>
+            /// Gets the SHA1 hash of <code>input</code>, and returns that hash. The input string is converted
+            /// using <code>encoding</code>.
+            /// </summary>
+            /// <param name="input">The input string</param>
+            /// <param name="encoding">The encoding to be used for the conversion.</param>
+            /// <returns>Returns the hash as a 40-character hexadecimal number.</returns>
+            public static string GetSha1Hash(string input, Encoding encoding) {
+                using (SHA1 sha1 = SHA1.Create()) return GetHash(sha1, input, encoding);
+            }
+
+            /// <summary>
+            /// Gets the SHA1 hash of the file specified by the <code>path</code>. The hash is a 40-character
+            /// hexadecimal number.
+            /// </summary>
+            /// <param name="path">The path of the file.</param>
+            /// <returns>Returns the hash as a 40-character hexadecimal number.</returns>
+            public static string GetSha1FromFile(string path) {
+                using (SHA1 sha1 = SHA1.Create()) return GetHashFromFile(sha1, path);
+            }
+
+            /// <summary>
+            /// Gets the SHA256 hash of <code>input</code>, and returns that hash. The input string is converted using <see cref="Encoding.UTF8"/>.
+            /// </summary>
+            /// <param name="input">The input string</param>
+            /// <returns>Returns the hash as a 64-character hexadecimal number.</returns>
+            public static string GetSha256Hash(string input) {
+                using (SHA256 sha256 = SHA256.Create()) return GetHash(sha256, input);
+            }
+
+            /// <summary>
+            /// Gets the SHA256 hash of <code>input</code>, and returns that hash. The input string is converted using <code>encoding</code>.
+            /// </summary>
+            /// <param name="input">The input string</param>
+            /// <param name="encoding">The encoding to be used for the conversion.</param>
+            /// <returns>Returns the hash as a 64-character hexadecimal number.</returns>
+            public static string GetSha256Hash(string input, Encoding encoding) {
+                using (SHA256 sha256 = SHA256.Create()) return GetHash(sha256, input, encoding);
+            }
+
+            /// <summary>
+            /// Gets the SHA256 hash of the file specified by the <code>path</code>. The hash is a 64-character
+            /// hexadecimal number.
+            /// </summary>
+            /// <param name="path">The path of the file.</param>
+            /// <returns>Returns the hash as a 64-character hexadecimal number.</returns>
+            public static string GetSha256HashFromFile(string path) {
+                using (SHA256 sha256 = SHA256.Create()) return GetHashFromFile(sha256, path);
+            }
+
+            /// <summary>
+            /// Gets the SHA512 hash of <code>input</code>, and returns that hash. The input string is converted using <see cref="Encoding.UTF8"/>.
+            /// </summary>
+            /// <param name="input">The input string</param>
+            /// <returns>Returns the hash as a 128-character hexadecimal number.</returns>
+            public static string GetSha512Hash(string input) {
+                using (SHA512 sha512 = SHA512.Create()) return GetHash(sha512, input);
+            }
+
+            /// <summary>
+            /// Gets the SHA512 hash of <code>input</code>, and returns that hash. The input string is converted using <code>encoding</code>.
+            /// </summary>
+            /// <param name="input">The input string</param>
+            /// <param name="encoding">The encoding to be used for the conversion.</param>
+            /// <returns>Returns the hash as a 128-character hexadecimal number.</returns>
+            public static string GetSha512Hash(string input, Encoding encoding) {
+                using (SHA512 sha512 = SHA512.Create()) return GetHash(sha512, input, encoding);
+            }
+
+            /// <summary>
+            /// Gets the SHA512 hash of the file specified by the <code>path</code>. The hash is a 128-character
+            /// hexadecimal number.
+            /// </summary>
+            /// <param name="path">The path of the file.</param>
+            /// <returns>Returns the hash as a 128-character hexadecimal number.</returns>
+            public static string GetSha512HashFromFile(string path) {
+                using (SHA512 sha512 = SHA512.Create()) return GetHashFromFile(sha512, path);
+            }
+
+            /// <summary>
+            /// Gets the hash of <code>input</code> using the specified <code>algorithm</code>.
+            /// </summary>
+            /// <param name="algorithm">The algorithm to be used.</param>
+            /// <param name="input">The input string to be hashed.</param>
+            /// <returns>Returns the hash as a hexadecimal number.</returns>
+            public static string GetHash(HashAlgorithm algorithm, string input) {
+                return GetHash(algorithm, input, Encoding.UTF8);
+            }
+
+            /// <summary>
+            /// Gets the hash of <code>input</code> using the specified <code>algorithm</code>.
+            /// </summary>
+            /// <param name="algorithm">The algorithm to be used.</param>
+            /// <param name="input">The input string to be hashed.</param>
+            /// <param name="encoding">The encoding to be used.</param>
+            /// <returns>Returns the hash as a hexadecimal number.</returns>
+            public static string GetHash(HashAlgorithm algorithm, string input, Encoding encoding) {
+                return BitConverter.ToString(algorithm.ComputeHash(encoding.GetBytes(input))).Replace("-", "").ToLower();
+            }
+
+            /// <summary>
+            /// Gets the hash of the file specified by the <code>path</code> using the specified <code>algorithm</code>.
+            /// </summary>
+            /// <param name="algorithm">The algorithm to be used.</param>
+            /// <param name="path">The path of the file.</param>
+            /// <returns>Returns the hash as a 64-character hexadecimal number.</returns>
+            public static string GetHashFromFile(HashAlgorithm algorithm, string path) {
+                using (FileStream stream = File.OpenRead(path)) {
+                    return BitConverter.ToString(algorithm.ComputeHash(stream)).Replace("-", "").ToLower();
+                }
+            }
+
         }
     
     }
--- src/UnitTestProject1/Utils/Security.cs
+++ src/UnitTestProject1/Utils/Security.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Skybrud.Social;
 
 namespace UnitTestProject1.Utils {
@@ -31,7 +32,67 @@ namespace UnitTestProject1.Utils {
             foreach (var sample in samples) {
                 Assert.AreEqual(sample.Expected, SocialUtils.Security.Base64Decode(sample.Input));
             }
+        
+        }
 
+        [TestMethod]
+        public void GetMd5Hash() {
+
+            var samples = new[] {
+                new { Input = "Hello World", Expected = "b10a8db164e0754105b7a99be72e3fe5" },
+                new { Input = "Rød grød med fløde", Expected = "64d6f69ac402593bc004c8adf3db4b22" }
+            };
+
+            foreach (var sample in samples) {
+                Assert.AreEqual(sample.Expected, SocialUtils.Security.GetMd5Hash(sample.Input), "Failed hashing " + sample.Input + " (no encoding)");
+                Assert.AreEqual(sample.Expected, SocialUtils.Security.GetMd5Hash(sample.Input, Encoding.UTF8), "Failed hashing " + sample.Input + " (UTF8)");
+            }
+        
+        }
+
+        [TestMethod]
+        public void GetSha1Hash() {
+
+            var samples = new[] {
+                new { Input = "Hello World", Expected = "0a4d55a8d778e5022fab701977c5d840bbc486d0" },
+                new { Input = "Rød grød med fløde", Expected = "20a219e95c358780bfc974bd47ae13ba49a6e871" }
+            };
+
+            foreach (var sample in samples) {
+                Assert.AreEqual(sample.Expected, SocialUtils.Security.GetSha1Hash(sample.Input), "Failed hashing " + sample.Expected + " (no encoding)");
+                Assert.AreEqual(sample.Expected, SocialUtils.Security.GetSha1Hash(sample.Input, Encoding.UTF8), "Failed hashing " + sample.Expected + " (UTF8)");
+            }
+        
+        }
+
+        [TestMethod]
+        public void GetSha256Hash() {
+
+            var samples = new[] {
+                new { Input = "Hello World", Expected = "a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e" },
+                new { Input = "Rød grød med fløde", Expected = "16037d3adef43eb338b1b96bb6ee1211c5906bdb671a5aae92be62a744ada72b" }
+            };
+
+            foreach (var sample in samples) {
+                Assert.AreEqual(sample.Expected, SocialUtils.Security.GetSha256Hash(sample.Input), "Failed hashing " + sample.Expected + " (no encoding)");
+                Assert.AreEqual(sample.Expected, SocialUtils.Security.GetSha256Hash(sample.Input, Encoding.UTF8), "Failed hashing " + sample.Expected + " (UTF8)");
+            }
+        
+        }
+
+        [TestMethod]
+        public void GetSha512Hash() {
+
+            var samples = new[] {
+                new { Input = "Hello World", Expected = "2c74fd17edafd80e8447b0d46741ee243b7eb74dd2149a0ab1b9246fb30382f27e853d8585719e0e67cbda0daa8f51671064615d645ae27acb15bfb1447f459b" },
+                new { Input = "Rød grød med fløde", Expected = "23d6e34e1a04aff3c4971d9666d4b4e490dda54ea4241083dc4f18f9bd813333de4731c90ea6c9b7913cc42cd0a9f5b8bd734b07cad5fb5359de4db3e9bb7476" }
+            };
+
+            foreach (var sample in samples) {
+                Assert.AreEqual(sample.Expected, SocialUtils.Security.GetSha512Hash(sample.Input), "Failed hashing " + sample.Expected + " (no encoding)");
+                Assert.AreEqual(sample.Expected, SocialUtils.Security.GetSha512Hash(sample.Input, Encoding.UTF8), "Failed hashing " + sample.Expected + " (UTF8)");
+            }
+        
         }
 
     }
