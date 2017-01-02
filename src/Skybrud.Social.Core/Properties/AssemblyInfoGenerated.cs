6303a1047427ef56351c43274637b4abecd579ea
Anders Bjerner <abjerner@skybrud.dk>
2017-01-02 21:07:54 +0100

    New beta release :D

--- src/Skybrud.Social.Core/Properties/AssemblyInfoGenerated.cs
+++ src/Skybrud.Social.Core/Properties/AssemblyInfoGenerated.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyFileVersion("0.0.1582.2")]
+[assembly: AssemblyFileVersion("0.0.1617.1")]
 
--- src/Skybrud.Social.Core/Properties/AssemblyInfoVersion.cs
+++ src/Skybrud.Social.Core/Properties/AssemblyInfoVersion.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta5")]
+[assembly: AssemblyInformationalVersion("1.0.0-beta6")]
 [assembly: AssemblyMetadata("StartDate", "2012-07-30")]
